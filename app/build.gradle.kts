8a091eea63ac0de71bae79a15e623763fe5bca50
AdrianJavier7 <aperezmiranda@safareyes.es>
2024-11-30 19:15:11 +0100

    Menú hecho

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("com.google.android.material:material:1.9.0")
+
 }
