f5ca877948432254722971c544e08d97a0c9e079
Angelitroo <amedinillaandrada@safareyes.es>
2024-12-03 11:25:34 +0100

    V3-Vista Amigos terminá

--- app/src/main/java/com/example/proyectokotlinense/MainActivity.kt
+++ app/src/main/java/com/example/proyectokotlinense/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.balances)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
